b58bfa4bfac08c58fdd802c26c59529802ab6747
I M Sohail <tuneinsohail@gmail.com>
2018-08-14 00:17:27 +0530

    basic version of source codes

---
+++ index.js
@@ -0,0 +1,22 @@
+i<!DOCTYPE html>
+<title>My Example</title>
+
+
+<p id="msg"></p>
+
+<script>
+  // Set variables
+  var myBankBalance = 0;
+  var output = "";
+
+  // Do the 'for' loop
+  for (myBankBalance = 0; myBankBalance <= 10; myBankBalance++) {
+    output += "My bank balance is now $" + myBankBalance + "<br>";
+  }
+
+  // Output results to the above HTML element
+  document.getElementById("msg").innerHTML = output;
+/script>
+
+the class has begun and is being educative
+.js program
